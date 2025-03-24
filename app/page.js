4507c9e54e5d9bdff57b4ff1a46aa16d5e1371d2
OWAIS KHAN <khan.owais0555@gmail.com>
2025-03-24 23:47:20 +0530

    changing commit msg

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>hello</h2>
+      <h2>hello Buddy</h2>
       <Button variant={'destructive'}>Click here</Button>
     </div>
   );
