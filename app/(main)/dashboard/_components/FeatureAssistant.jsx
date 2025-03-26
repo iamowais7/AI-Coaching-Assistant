9e03662c73bdfa5470ffe1a396deba6451c0b26b
OWAIS KHAN <khan.owais0555@gmail.com>
2025-03-27 04:05:35 +0530

    Adding dashboard functionality for logged-in user

---
+++ app/(main)/dashboard/_components/FeatureAssistant.jsx
@@ -0,0 +1,37 @@
+"use client"
+import { BlurFade } from '@/components/magicui/blur-fade'
+import { Button } from '@/components/ui/button'
+import { CoachingOptions } from '@/services/Options'
+import { useUser } from '@stackframe/stack'
+import Image from 'next/image'
+import React from 'react'
+
+function FeatureAssistant() {
+
+    const user = useUser()
+  return (
+    <div>
+        <div className='flex justify-between items-center'>
+        <div>
+        <h2 className='font-medium text-gray-500'>My Workspace</h2>
+        <h2 className='text-3xl font-bold'>Welcome back, {user?.displayName}</h2>
+        </div>
+        <Button className="bg-[linear-gradient(45deg,_#64C2DB,_#7476ED,_#C994DF,_#E56F8C)]">Profile</Button>
+        </div>
+
+        <div className='grid grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-5 mt-7'>
+            {CoachingOptions.map((option,index)=>(
+                 <BlurFade key={option.icon} delay={0.25 + index * 0.05} inView>
+                <div key={index} className='p-3 bg-secondary rounded-xl flex flex-col justify-center items-center'>
+                    <Image src={option.icon} alt='option logo'
+                     height={150} width={150} className='h-[70px] w-[70px] hover:rotate-12 cursor-pointer transition-all'/>
+                     <h2 className='mt-2'>{option.name}</h2>
+                </div>
+                </BlurFade>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default FeatureAssistant 
---
+++ app/(main)/dashboard/_components/Feedback.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Feedback() {
+  return (
+    <div>
+        <h2 className='font-black text-xl'>Feedback</h2>
+        <h2 className='text-gray-400'>You don't have any interview feedback</h2>
+
+
+    </div>
+  )
+}
+
+export default Feedback
