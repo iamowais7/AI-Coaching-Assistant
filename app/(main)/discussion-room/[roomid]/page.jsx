99aceb4a46101260f4d0da4b0dabf30efd54ef87
OWAIS KHAN <khan.owais0555@gmail.com>
2025-04-02 23:10:57 +0530

    Fixed some DB bugs

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DiscussionRoom () {
   return (
-    <div className='text-3xl'>DiscussionRoom images  </div>
+    <div className='text-3xl border-black cursor-pointer'>DiscussionRoom images  </div>
   )
 }
 
