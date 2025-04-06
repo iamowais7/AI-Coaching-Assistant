0ae82c766d6412cf4240e0babd89c44de0c94526
OWAIS KHAN <khan.owais0555@gmail.com>
2025-04-06 22:55:57 +0530

    Microphone setup completed

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -5,14 +5,20 @@ import { api } from '@/convex/_generated/api'
 import { CoachingExpert } from '@/services/Options'
 import { UserButton } from '@stackframe/stack'
 import { useQuery } from 'convex/react'
+import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+// import RecordRTC from 'recordrtc'
+const RecordRTC = dynamic(()=>import("recordrtc"),{ssr:false});
 
 function DiscussionRoom () {
   const {roomid} = useParams()
   const DiscussionRoomData = useQuery(api.DiscussionRoom.GetDiscussionRoom,{id:roomid});
   const [expert,setExpert] = useState();
+  const [enableMic,setEnablemic] = useState()
+  const recorder = useRef(null)
+  let silenceTimeout;
 
   useEffect(()=>{
     if(DiscussionRoomData){
@@ -22,6 +28,48 @@ function DiscussionRoom () {
     }
   },[DiscussionRoomData])
 
+  const connectToServer = async () => {
+    setEnablemic(true);
+    if (typeof window !== "undefined" && typeof navigator !== "undefined") {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  
+        const { default: RecordRTC, StereoAudioRecorder } = await import("recordrtc");
+  
+        recorder.current = new RecordRTC(stream, {
+          type: 'audio',
+          mimeType: 'audio/webm;codecs=pcm',
+          recorderType: StereoAudioRecorder,
+          timeSlice: 250,
+          desiredSampRate: 16000,
+          numberOfAudioChannels: 1,
+          bufferSize: 4096,
+          audioBitsPerSecond: 128000,
+          ondataavailable: async (blob) => {
+            clearTimeout(silenceTimeout);
+            const buffer = await blob.arrayBuffer();
+            silenceTimeout = setTimeout(() => {
+              console.log('User stopped talking');
+            }, 2000);
+          },
+        });
+  
+        recorder.current.startRecording();
+        console.log("current data", recorder.current);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+  
+
+  const disconnect=(e)=>{
+      e.preventDefault();
+      recorder.current.pauseRecording();
+      recorder.current=null;
+      setEnablemic(false);
+  }
+
   return (
     <div className='-mt-15'>
       <h2 className='text-lg font-bold'>{DiscussionRoomData?.coachingOption}</h2>
@@ -29,8 +77,10 @@ function DiscussionRoom () {
         <div className='lg:col-span-2'>
         <div className='lg:col-span-2 h-[60vh] bg-secondary border rounded-4xl
          flex flex-col items-center justify-center relative'>
-            <Image src={expert?.avatar} alt='Avatar' height={200} width={200}
-                   className='h-[80px] w-[80px] rounded-full object-cover animate-pulse'/>
+          {expert?.avatar ?(
+              <Image src={expert.avatar} alt='Avatar' height={200} width={200}
+              className='h-[80px] w-[80px] rounded-full object-cover animate-pulse'/>
+          ):null}  
             <h2 className='text-gray-500'>{expert?.name}</h2>   
 
             <div className='p-5 bg-gray-200 px-10 rounded-lg absolute bottom-10 right-10 '>
@@ -38,7 +88,9 @@ function DiscussionRoom () {
               </div>    
         </div>
         <div className='mt-5 flex justify-center items-center'>
-          <Button className="bg-[linear-gradient(45deg,_#64C2DB,_#7476ED,_#C994DF,_#E56F8C)]">Connect</Button>
+          {!enableMic?
+          <Button onClick={connectToServer} className="bg-[linear-gradient(45deg,_#64C2DB,_#7476ED,_#C994DF,_#E56F8C)] cursor-pointer">Connect</Button>
+          : <Button variant={'destructive'} onClick={disconnect} className={'cursor-pointer'}>Disconnect</Button>}
         </div>
         </div>
         <div>
